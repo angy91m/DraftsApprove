d32381433df71b85d93764904b5c5f90ff0ea9af
Angelo Burzi <angelo.burzi@immobiliare.it>
2024-11-12 18:09:53 +0100

    updated

--- includes/DraftHooks.php
+++ includes/DraftHooks.php
@@ -219,7 +219,7 @@ class DraftHooks {
 	}
 
 	public function visualEditorBeforeInitialize( &$title, $unused, $output, $user, $request, $mediawiki ) {
-		hSaveTest($title);
+		hSaveTest('CIAO');
 		if ( $request->getVal( 'veaction' ) && !$user->isAllowed('drafts-approve') ) {
 			$output->showErrorPage("apierror-approvedrafts-permissions");
 			return false;
